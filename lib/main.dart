f82613b0f0841d154e410207f7cecbd58e0a335c
syedzeeshanahmed <s.zeeshan47@gmail.com>
2023-08-23 21:26:40 +0500

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          scaffoldBackgroundColor: const Color.fromARGB(255, 242, 248, 254)),
       home: HomePage(),
     );
   }
@@ -23,46 +25,46 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            //Header Container
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.blue,
-              alignment: Alignment.center,
-              child: const Text("Header"),
-            ),
-
             //Body Container
             Expanded(
               child: SingleChildScrollView(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
                 child: Column(
                   children: <Widget>[
                     Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("assets/images/splashBG.png"),
+                              fit: BoxFit.cover)),
                       alignment: Alignment.center,
-                      padding: const EdgeInsets.only(top: 30, bottom: 20),
-                      child: const Text("Sign In",
-                          style: TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Cutive')),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      child: const MyCustomForm(),
+                      padding: const EdgeInsets.only(top: 90, bottom: 20),
+                      child: Image.asset('assets/images/logo.png', width: 150),
                     )
                   ],
                 ),
               ),
             ),
 
-            //Footer Container
-            //Here you will get unexpected behaviour when keyboard pops-up.
-            //So its better to use `bottomNavigationBar` to avoid this.
+            //Form Container
             Container(
-              padding: const EdgeInsets.all(8.0),
-              color: Colors.blue,
+              padding: const EdgeInsets.only(top: 40, bottom: 20),
               alignment: Alignment.center,
-              child: const Text("Footer"),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(40),
+                    topLeft: Radius.circular(40),
+                  )),
+              child: const Text("Sign In",
+                  style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Cutive')),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              color: Colors.white,
+              alignment: Alignment.center,
+              child: const MyCustomForm(),
             ),
           ],
         ),
@@ -98,17 +100,31 @@ class MyCustomFormState extends State<MyCustomForm> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              padding: const EdgeInsets.only(bottom: 0, left: 10),
+              padding: const EdgeInsets.only(bottom: 10, left: 10),
               alignment: Alignment.topLeft,
-              child: const Text("Enter your Email",
-                  style: TextStyle(fontSize: 14.0)),
+              child: const Text("Email", style: TextStyle(fontSize: 13.0)),
             ),
             Container(
-              padding: const EdgeInsets.all(8.0),
+              // padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(bottom: 20),
               alignment: Alignment.center,
               child: TextFormField(
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Enter Email'),
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  hintText: 'Hello@yahoo.com',
+                  fillColor: const Color.fromARGB(255, 243, 249, 255),
+                  // fillColor: Colors.deepPurpleAccent,
+                ),
+                style: const TextStyle(fontSize: 13),
                 validator: (val) {
                   if (val == null || val.isEmpty || !val.contains("@")) {
                     return 'Enter a valid eamil';
@@ -118,20 +134,31 @@ class MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(top: 15, bottom: 0, left: 10),
+              padding: const EdgeInsets.only(top: 10, bottom: 10, left: 10),
               alignment: Alignment.topLeft,
-              child: const Text("Enter your password",
-                  style: TextStyle(fontSize: 14.0)),
+              child: const Text("Password", style: TextStyle(fontSize: 13.0)),
             ),
             Container(
               padding: const EdgeInsets.all(8.0),
               alignment: Alignment.center,
               child: TextFormField(
                 obscureText: true,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: "Password",
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                  filled: true,
+                  hintStyle: TextStyle(color: Colors.grey[800]),
+                  labelText: "Password",
+                  fillColor: const Color.fromARGB(255, 243, 249, 255),
+                  // fillColor: Colors.deepPurpleAccent,
                 ),
+                style: const TextStyle(fontSize: 13),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Please insert password';
@@ -141,9 +168,12 @@ class MyCustomFormState extends State<MyCustomForm> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.only(top: 20, bottom: 30),
               alignment: Alignment.center,
               child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black, //<-- SEE HERE
+                    minimumSize: const Size.fromHeight(60)),
                 onPressed: () {
                   // Validate returns true if the form is valid, or false otherwise.
                   if (_formKey.currentState!.validate()) {
